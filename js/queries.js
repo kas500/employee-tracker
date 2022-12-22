d2238f6fd6b5c44113930f8c2d5911f6ca9f6613
Anton Krasnikov <krasnikovanton84@gmail.com>
2022-12-21 22:19:24 -0800

    Modifies queries

--- js/queries.js
+++ js/queries.js
@@ -1,3 +1,4 @@
+const { connectable } = require("rxjs");
 const {db} = require("./connection");
 
 class QueryFrom{
@@ -6,24 +7,29 @@ class QueryFrom{
     }
 
     //get all employees
-    getAllEmployees(){
-        return this.db.query('select * from employee;', async function (err, results) {
-            console.log(results);
-        });
+    async getAllEmployees(){
+        return await this.db.query(
+        `select a.id,
+        a.first_name,
+        a.last_name,
+        title,
+        department.name as department,
+        salary,
+        concat(b.first_name, ' ',b.last_name) as manager
+        from employee a
+        left join employee b on b.id = a.manager_id
+        inner join role on role.id = a.role_id
+        join department on department.id = role.department_id;`);
     }
 
     //get all departments
-    getAllDepartments(){
-        return this.db.query('select * from department;', async function (err, results) {
-            console.log(results);
-        });
+    async getAllDepartments(){
+        return await this.db.query('select * from department;')
     }
 
     //get all roles
-    getAllRoles(){
-        return this.db.query('select * from role;', async function (err, results) {
-            console.log(results);
-        });
+    async getAllRoles(){
+        return await this.db.query('select * from role;');
     }
 
 }   
