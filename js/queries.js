ce5bc70aea0fa6c368f959bf9102cc14c71deeae
Anton Krasnikov <krasnikovanton84@gmail.com>
2022-12-21 21:34:55 -0800

    Adds main gets to cobnstructor

--- js/queries.js
+++ js/queries.js
@@ -1,12 +1,31 @@
-const db = require("./connection");
+const {db} = require("./connection");
 
-class Query{
+class QueryFrom{
     constructor(db){
         this.db = db;
     }
 
     //get all employees
     getAllEmployees(){
-        return this.db.query("select * from employee");
+        return this.db.query('select * from employee;', async function (err, results) {
+            console.log(results);
+        });
     }
-}
+
+    //get all departments
+    getAllDepartments(){
+        return this.db.query('select * from department;', async function (err, results) {
+            console.log(results);
+        });
+    }
+
+    //get all roles
+    getAllRoles(){
+        return this.db.query('select * from role;', async function (err, results) {
+            console.log(results);
+        });
+    }
+
+}   
+
+module.exports = new QueryFrom(db);
